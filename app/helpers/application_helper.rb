2a96e52085d2899ffaeebac484e55a462a64be16
Dan Rufener <dan@deckmonitoring.com>
2013-09-23 23:50:15 -0700

    Only count uniq meals towards count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def num_recipes_in_current_meal_plan
-    session[:recipe_ids].try(:split, ',').try(:size).to_i
+    session[:recipe_ids].try(:split, ',').try(:uniq).try(:size).to_i
   end
 end

51af2435f7bcc2199b2c7f831cde91a210d412f6
Dan Rufener <dan@deckmonitoring.com>
2013-09-23 21:21:49 -0700

    Make recipes#index the root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Mealplan::Application.routes.draw do
       get 'remove_recipes'
     end
   end
+
+  root 'recipes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

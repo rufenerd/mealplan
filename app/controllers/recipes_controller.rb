a08b88dd61180808fad9c5ce0054ebc10c93de05
Dan Rufener <dan@deckmonitoring.com>
2013-10-19 16:41:44 -0700

    recipe edit, but no links and cant remove recipe_ingredients but it looks like you can

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,21 +7,31 @@ class RecipesController < ApplicationController
   def create
     r = Recipe.new(recipe_params)
     if r.save
-      params[:recipe_ingredients].each do |num, ri|
-        next if ri[:quantity].blank? && ri[:ingredient_id].blank?
-        match = ri[:ingredient_id].match(/NEWINGREDIENT\[(.+)\]/)
-        if match
-          i = Ingredient.find_by_name(match[1].strip) || Ingredient.create( name: match[1] )
-          ri.merge!(ingredient_id: i.id)
-        end
-        RecipeIngredient.create(ri.merge(recipe_id: r.id))
-      end
+      process_ingredient_params(r)
       flash[:notice] = "Successfully created new recipe"
     else
       flash[:alert] = r.errors.full_messages.join(" | ")
       flash.keep
     end
     redirect_to :action => :new
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @title = "Edit #{@recipe.name}"
+  end
+
+  def update
+    r = Recipe.find(params[:id])
+    if r.update_attributes(params.require(:recipe).permit(:name, :instructions, :text))
+      process_ingredient_params(r)
+      flash[:notice] = "Successfully edited #{r.name}"
+      redirect_to :action => :index
+    else
+      flash[:alert] = r.errors.full_messages.join(" | ")
+      flash.keep
+      redirect_to :action => :edit
+    end    
   end
 
   def show
@@ -57,4 +67,20 @@ class RecipesController < ApplicationController
     params.require(:recipe).permit(:name, :text)
   end
 
+  def process_ingredient_params(recipe)
+    params[:recipe_ingredients].each do |num, ri|
+      next if ri[:quantity].blank? && ri[:ingredient_id].blank?
+      match = ri[:ingredient_id].match(/NEWINGREDIENT\[(.+)\]/)
+      if match
+        i = Ingredient.find_by_name(match[1].strip) || Ingredient.create( name: match[1] )
+        ri.merge!(ingredient_id: i.id)
+      end
+
+      if ri[:id].blank?
+        RecipeIngredient.create(ri.merge(recipe_id: recipe.id))
+      else
+        RecipeIngredient.find(ri[:id].to_i).update_attributes(ri)
+      end
+    end
+  end
 end

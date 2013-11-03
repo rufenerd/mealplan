cb30b4b3d9e3b8da2636d93592656d936cfc3edf
Dan Rufener <dan@deckmonitoring.com>
2013-11-02 17:41:52 -0700

    Dont suggest adding to current meal plan if already in it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def num_recipes_in_current_meal_plan
-    return 0 if session[:recipe_ids].blank?
-    session[:recipe_ids].split(',').uniq.size
+  def recipe_ids_in_current_meal_plan
+    return [] if session[:recipe_ids].blank?
+    session[:recipe_ids].split(',').map(&:to_i)
   end
 end
